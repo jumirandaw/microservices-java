7882cfc779b5439463f6a7a42b0f6dfb713d2650
Júlia Miranda <juliaammiranda@gmail.com>
2025-06-02 20:48:07 -0300

    Começando a inserir a Gateway

---
+++ gateway-service/src/main/java/br/edu/atitus/gateway_service/configs/GatewayConfig.java
@@ -0,0 +1,30 @@
+package br.edu.atitus.gateway_service.configs;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+	
+	
+	@Bean
+	RouteLocator getRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route(p -> p
+					.path("/get")
+					.filters(f -> f
+							.addRequestHeader("X-UBER-ID", "1234")
+							.addRequestParameter("env", "dev"))
+					.uri("http://httpbin.org:80"))
+			.route(p -> p
+					.path("/currency/**")
+					.uri("lb://currency-service"))
+			.route(p -> p
+					.path("/products/**")
+					.uri("lb://product-service"))
+			.build();
+	}
+
+}
